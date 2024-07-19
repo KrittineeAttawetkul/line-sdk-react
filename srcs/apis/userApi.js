98905f8b34f9b20f63f660267d8ba6ccf3d3ec16
Krittinee <krittinee.atta@gmail.com>
2024-07-19 11:56:09 +0700

    ngrok-skip-browser-warning

--- srcs/apis/userApi.js
+++ srcs/apis/userApi.js
@@ -8,11 +8,13 @@ export const USER_ACTION = {
 
 function getUserByUserId(payload) {
     return new Promise(async resolve => {
-        axios.post(BASE_URL.baseApi + '/api/users', payload, {
-            headers: {
-                "ngrok-skip-browser-warning": "69420"
-            }
-        })
+        axios.post(BASE_URL.baseApi + '/api/users', payload,
+            // {
+            //     headers: {
+            //         "ngrok-skip-browser-warning": "69420"
+            //     }
+            // }
+        )
             .then(res => {
                 console.log(res);
                 resolve(res.data);
